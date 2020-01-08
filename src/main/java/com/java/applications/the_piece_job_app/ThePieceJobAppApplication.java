da3271601b843ff9723b246ead6c69496881e4f2
Ngoveni <Matimu.Ngoveni@absa.africa>
2020-01-08 10:30:35 +0200

    Initial commit

---
+++ src/main/java/com/java/applications/the_piece_job_app/ThePieceJobAppApplication.java
@@ -0,0 +1,13 @@
+package com.java.applications.the_piece_job_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThePieceJobAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThePieceJobAppApplication.class, args);
+    }
+
+}
